2376eebee75a6f8c1f1076c03a528dc63f818003
Wilson Bracamonte <wd.bracamonte10@gmail.com>
2025-07-13 18:03:04 -0400

    State management using Redux

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./ProductList.css";
 import CartItem from "./CartItem";
-import addItem from "./CartSlice";
+import { addItem } from "./CartSlice";
 import { useDispatch } from "react-redux";
 function ProductList({ onHomeClick }) {
   const [showCart, setShowCart] = useState(false);
